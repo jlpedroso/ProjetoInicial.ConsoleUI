8757de2ef7effbc566fcbb2faab84aceed1f4c48
jlpedroso <jlpedroso@yahoo.com.br>
2017-10-27 21:04:08 +0000

    Add files via upload
    Projeto para testar a entrada de dados

---
+++ ListaDeFilmes.ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using ListaDeFilmes.Data.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeFilmes.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new FilmesDataContext())
+            {
+                var diretores = ctx.Diretores.ToList();
+                foreach (var diretor in diretores)
+                {
+                    Console.WriteLine("ID: {0} - Nome: {1}",
+                        diretor.Id, diretor.Nome
+                        );
+
+                }
+            }
+        }
+    }
+}
